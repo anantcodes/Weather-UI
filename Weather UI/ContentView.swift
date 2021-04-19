0310e15fdd04828de1963fa02739870a51fd97fd
Anant Kanchan <anant4265@gmail.com>
2021-04-19 20:03:15 +0530

    background colour added

---
+++ Weather UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather UI
+//
+//  Created by Anant Kanchan on 19/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading,
+                endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather UI/Weather_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_UIApp.swift
+//  Weather UI
+//
+//  Created by Anant Kanchan on 19/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
