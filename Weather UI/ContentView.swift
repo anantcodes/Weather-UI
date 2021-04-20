98b15febaf1d310ad3de83d70573d3df44e190b1
Anant Kanchan <anant4265@gmail.com>
2021-04-20 11:12:22 +0530

    sf symbol added

--- Weather UI/ContentView.swift
+++ Weather UI/ContentView.swift
@@ -18,6 +18,18 @@ struct ContentView: View {
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                VStack(spacing: 10){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                    
+                }
                     Spacer()
             }
         }
